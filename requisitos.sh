1a6fe69b8c2749791435eee35e1ff0557d1da896
caiolul <caiolucena.il@gmail.com>
2019-12-12 12:57:00 -0300

    trabalhando no sistema baseado em debian

--- requisitos.sh
+++ requisitos.sh
@@ -10,4 +10,14 @@ pip install django-extensions django-widget-tweaks python-decouple six
 
 #pip install psycopg2
 
+#pip3 refere-se a o pip do python3
+sudo pip3 install --upgrade pip
+# Instalando os requisitos
+sudo pip3 install Django 
+# Outras dependencias 
+sudo pip3 install django-extensions django-widget-tweaks python-decouple six
+
+# Para ultilizar o psycopg2 instale o postgres
+
+#pip install psycopg2
 
