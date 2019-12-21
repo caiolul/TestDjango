746748335b35dcdfec0ca5fa608abbe1eec12c71
caiolul <caiolucena.il@gmail.com>
2019-12-20 22:50:06 -0300

    ajustes no model e criação de superuser

--- myProject/core/models.py
+++ myProject/core/models.py
@@ -1,8 +1,15 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Quest (models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-# Create your models here.
 
+    def __str__(self):
+        return self.question_text
+
+    def pub(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
