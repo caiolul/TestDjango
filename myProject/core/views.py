38490f4d70aa417fe94452cb9a952bf4de82b77b
caiolul <caiolucena.il@gmail.com>
2019-12-22 21:38:33 -0300

    models no html

--- myProject/core/views.py
+++ myProject/core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from myProject.core.models import Quest
+from myProject.core.models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    post = Post.objects.filter(
+        pub_date__lte=timezone.now()).order_by('pub_date')
+    return render(request, 'index.html', {'post': post})
