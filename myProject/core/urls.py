1f26d80caa20930f9ae231e39eb661aa355d3bd9
caiolul <caiolucena.il@gmail.com>
2019-12-25 09:45:15 -0300

    paginas de posts concluidos

--- myProject/core/urls.py
+++ myProject/core/urls.py
@@ -3,5 +3,6 @@ from myProject.core import views as v
 
 urlpatterns = [
     path('', v.index, name='index'),
+    path('post/<int:pk>', v.post_pg, name='post_pg'),
 
 ]
--- myProject/core/views.py
+++ myProject/core/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from myProject.core.models import Post
 from django.utils import timezone
 
 # Create your views here.
 
+'''Post.objects.get(pk=pk)'''
 
 def index(request):
     posts = Post.objects.filter(
         pub_date__lte=timezone.now()).order_by('pub_date')
     return render(request, 'index.html', {'posts': posts})
+
+def post_pg(request, pk):
+    posts = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_pg.html', {'posts': posts})
