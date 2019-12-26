9bc4f244dcb808e2462f5fa8e4d5e9a5b80760ae
caiolul <caiolucena.il@gmail.com>
2019-12-26 12:29:01 -0300

    Erros no arquivo model e conflitos no DB

--- myProject/core/models.py
+++ myProject/core/models.py
@@ -9,6 +9,7 @@ class Post (models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    description = models.TextField()    
     created_date = models.DateTimeField(default=timezone.now)
     pub_date = models.DateTimeField(blank=True, null=True)
 
