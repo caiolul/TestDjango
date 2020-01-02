ded4e0c41ebc66c6f8ac36feaf596725929de1b7
caiolul <caiolucena.il@gmail.com>
2020-01-02 15:53:42 -0300

    testando heroku

--- myProject/settings.py
+++ myProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'uju!$z!acc#oid&m0&1+q6h+@-7ew=-paumwshm$84bcf=u+tv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'djangolul.herokuapp.com']
 
 
 # Application definition
